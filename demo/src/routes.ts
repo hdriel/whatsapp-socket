b2fa1bddc6fc107d94a334a9a75a402d06bc99c7
Hadriel Benjo <hdriel@gmail.com>
2025-12-28 00:24:44 +0200

    add client logic

--- demo/src/routes.ts
+++ demo/src/routes.ts
@@ -3,8 +3,8 @@ import path from 'pathe';
 import express, { type Request, type Response } from 'express';
 import { Server as SocketIO } from 'socket.io';
 import logger from './logger';
-// import { WhatsappSocket } from '@hdriel/whatsapp-socket';
-import { WhatsappSocket } from '../../src';
+import { WhatsappSocket } from '@hdriel/whatsapp-socket';
+// import { WhatsappSocket } from '../../src';
 import { uploadImage, uploadVideo, uploadAudio, uploadFile, uploadSticker } from './upload';
 
 export const initRouters = (io: SocketIO) => {
